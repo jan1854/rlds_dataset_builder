ef65aa70387483ad29ce1eec1f5f8cc318118a41
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2023-08-26 11:52:35 +0200

    Commented out invalid fields

--- pamy2_dynamic_motions/pamy2_dynamic_motions_dataset_builder.py
+++ pamy2_dynamic_motions/pamy2_dynamic_motions_dataset_builder.py
@@ -45,34 +45,34 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
                         dtype=np.float32,
                         doc='Robot action, consists of 8x desired muscle pressures (agonist 0, antagonist 0, agonist 1, ...).',
                     ),
-                    'discount': tfds.features.Scalar(
-                        dtype=np.float32,
-                        doc='There is no discount factor in the data, set to 1.'
-                    ),
-                    'reward': tfds.features.Scalar(
-                        dtype=np.float32,
-                        doc='There is no reward in the data, set to 0.'
-                    ),
-                    'is_first': tfds.features.Scalar(
-                        dtype=np.bool_,
-                        doc='The data is not divided into episodes, set to False.'
-                    ),
-                    'is_last': tfds.features.Scalar(
-                        dtype=np.bool_,
-                        doc='The data is not divided into episodes, set to False.'
-                    ),
-                    'is_terminal': tfds.features.Scalar(
-                        dtype=np.bool_,
-                        doc='The data is not divided into episodes, set to False.'
-                    ),
-                    'language_instruction': tfds.features.Text(
-                        doc='There are no language instructions in the data, set to a dummy value.'
-                    ),
-                    'language_embedding': tfds.features.Tensor(
-                        shape=(512,),
-                        dtype=np.float32,
-                        doc='There are no language instructions in the data, set to a dummy value.'
-                    ),
+                    # 'discount': tfds.features.Scalar(
+                    #     dtype=np.float32,
+                    #     doc='There is no discount factor in the data, set to 1.'
+                    # ),
+                    # 'reward': tfds.features.Scalar(
+                    #     dtype=np.float32,
+                    #     doc='There is no reward in the data, set to 0.'
+                    # ),
+                    # 'is_first': tfds.features.Scalar(
+                    #     dtype=np.bool_,
+                    #     doc='The data is not divided into episodes, set to False.'
+                    # ),
+                    # 'is_last': tfds.features.Scalar(
+                    #     dtype=np.bool_,
+                    #     doc='The data is not divided into episodes, set to False.'
+                    # ),
+                    # 'is_terminal': tfds.features.Scalar(
+                    #     dtype=np.bool_,
+                    #     doc='The data is not divided into episodes, set to False.'
+                    # ),
+                    # 'language_instruction': tfds.features.Text(
+                    #     doc='There are no language instructions in the data, set to a dummy value.'
+                    # ),
+                    # 'language_embedding': tfds.features.Tensor(
+                    #     shape=(512,),
+                    #     dtype=np.float32,
+                    #     doc='There are no language instructions in the data, set to a dummy value.'
+                    # ),
                 }),
                 'episode_metadata': tfds.features.FeaturesDict({
                     'file_path': tfds.features.Text(
@@ -102,9 +102,9 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
             # assemble episode --> here we're assuming demos so we set reward to 1 at the end
             episode = []
             for i, step in data.iterrows():
-                dummy_language_instruction = 'move dynamically'
+                # dummy_language_instruction = 'move dynamically'
                 # compute Kona language embedding
-                language_embedding = self._embed([dummy_language_instruction])[0].numpy()
+                # language_embedding = self._embed([dummy_language_instruction])[0].numpy()
                 state = data.loc[i, state_columns].values.astype(np.float32)
                 action = data.loc[i, action_columns].values.astype(np.float32)
 
@@ -114,20 +114,20 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
                         'state': state,
                     },
                     'action': action,
-                    'discount': 1.0,
-                    'reward': 0.0,
-                    'is_first': False,      # TODO: Should I set this?
-                    'is_last': False,
-                    'is_terminal': False,
-                    'language_instruction': dummy_language_instruction,
-                    'language_embedding': language_embedding,
+                    # 'discount': 1.0,
+                    # 'reward': 0.0,
+                    # 'is_first': False,
+                    # 'is_last': False,
+                    # 'is_terminal': False,
+                    # 'language_instruction': dummy_language_instruction,
+                    # 'language_embedding': language_embedding,
                 })
 
             # create output data sample
             sample = {
                 'steps': episode,
                 'episode_metadata': {
-                    'file_path': episode_path
+                    'file_path': episode_path.name
                 }
             }
 
@@ -139,7 +139,7 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
 
         # for smallish datasets, use single-thread parsing
         for sample in episode_paths:
-            yield _parse_example(sample)
+            yield _parse_example(Path(sample))
 
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
         # beam = tfds.core.lazy_imports.apache_beam
