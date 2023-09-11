0803b33d6d04191b678366c301f90cc9268d0154
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2023-09-11 09:15:40 +0200

    Resolved merge conflict

--- pamy2_dynamic_motions/pamy2_dynamic_motions_dataset_builder.py
+++ pamy2_dynamic_motions/pamy2_dynamic_motions_dataset_builder.py
@@ -19,7 +19,7 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self._embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
+        self._embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
 
     def _info(self) -> tfds.core.DatasetInfo:
         """Dataset metadata (homepage, citation,...)."""
@@ -45,34 +45,34 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
                         dtype=np.float32,
                         doc='Robot action, consists of 8x desired muscle pressures (agonist 0, antagonist 0, agonist 1, ...).',
                     ),
-                    # 'discount': tfds.features.Scalar(
-                    #     dtype=np.float32,
-                    #     doc='There is no discount factor in the data, set to 1.'
-                    # ),
-                    # 'reward': tfds.features.Scalar(
-                    #     dtype=np.float32,
-                    #     doc='There is no reward in the data, set to 0.'
-                    # ),
-                    # 'is_first': tfds.features.Scalar(
-                    #     dtype=np.bool_,
-                    #     doc='The data is not divided into episodes, set to False.'
-                    # ),
-                    # 'is_last': tfds.features.Scalar(
-                    #     dtype=np.bool_,
-                    #     doc='The data is not divided into episodes, set to False.'
-                    # ),
-                    # 'is_terminal': tfds.features.Scalar(
-                    #     dtype=np.bool_,
-                    #     doc='The data is not divided into episodes, set to False.'
-                    # ),
-                    # 'language_instruction': tfds.features.Text(
-                    #     doc='There are no language instructions in the data, set to a dummy value.'
-                    # ),
-                    # 'language_embedding': tfds.features.Tensor(
-                    #     shape=(512,),
-                    #     dtype=np.float32,
-                    #     doc='There are no language instructions in the data, set to a dummy value.'
-                    # ),
+                    'discount': tfds.features.Scalar(
+                        dtype=np.float32,
+                        doc='There is no discount factor in the data, set to 1.'
+                    ),
+                    'reward': tfds.features.Scalar(
+                        dtype=np.float32,
+                        doc='There is no reward in the data, set to 0.'
+                    ),
+                    'is_first': tfds.features.Scalar(
+                        dtype=np.bool_,
+                        doc='The data is not divided into episodes, set to False.'
+                    ),
+                    'is_last': tfds.features.Scalar(
+                        dtype=np.bool_,
+                        doc='The data is not divided into episodes, set to False.'
+                    ),
+                    'is_terminal': tfds.features.Scalar(
+                        dtype=np.bool_,
+                        doc='The data is not divided into episodes, set to False.'
+                    ),
+                    'language_instruction': tfds.features.Text(
+                        doc='There are no language instructions in the data, set to a dummy value.'
+                    ),
+                    'language_embedding': tfds.features.Tensor(
+                        shape=(512,),
+                        dtype=np.float32,
+                        doc='There are no language instructions in the data, set to a dummy value.'
+                    ),
                 }),
                 'episode_metadata': tfds.features.FeaturesDict({
                     'file_path': tfds.features.Text(
@@ -91,6 +91,10 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
         """Generator of examples for each split."""
 
         def _parse_example(episode_path):
+            dummy_language_instruction = 'move dynamically'
+            # compute Kona language embedding
+            language_embedding = self._embed([dummy_language_instruction])[0].numpy()
+
             # load raw data --> this should change for your dataset
             data = pd.read_pickle(episode_path)
 
@@ -116,13 +120,13 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
                         'state': state,
                     },
                     'action': action,
-                    # 'discount': 1.0,
-                    # 'reward': 0.0,
-                    # 'is_first': False,
-                    # 'is_last': False,
-                    # 'is_terminal': False,
-                    # 'language_instruction': dummy_language_instruction,
-                    # 'language_embedding': language_embedding,
+                    'discount': 1.0,
+                    'reward': 0.0,
+                    'is_first': False,
+                    'is_last': False,
+                    'is_terminal': False,
+                    'language_instruction': dummy_language_instruction,
+                    'language_embedding': language_embedding,
                 })
 
             # create output data sample
