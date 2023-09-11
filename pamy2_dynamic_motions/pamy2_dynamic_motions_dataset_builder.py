71a2aaca76ed949d0ec8f0ea27a9610a72556b02
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2023-09-11 16:55:23 +0200

    Fixed a problem where episode_path was a string instead of a Path

--- pamy2_dynamic_motions/pamy2_dynamic_motions_dataset_builder.py
+++ pamy2_dynamic_motions/pamy2_dynamic_motions_dataset_builder.py
@@ -154,7 +154,7 @@ class Pamy2DynamicMotions(tfds.core.GeneratorBasedBuilder):
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
         beam = tfds.core.lazy_imports.apache_beam
         return (
-                beam.Create(episode_paths)
+                beam.Create(Path(episode_paths))
                 | beam.Map(_parse_example)
         )
 
